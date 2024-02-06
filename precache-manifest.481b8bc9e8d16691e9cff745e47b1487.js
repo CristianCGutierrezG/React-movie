9f5c2bf4e5831a2a3608f694ba0944b9d5a1bf17
Cristian <cris123.gut@gmail.com>
2024-02-06 14:22:31 -0500

    Updates

---
+++ precache-manifest.481b8bc9e8d16691e9cff745e47b1487.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c686d7d9d0165a1ba6b8",
+    "url": "/React-movie/static/js/main.f0e235ce.chunk.js"
+  },
+  {
+    "revision": "4dd4cf68cf4c88540ea1",
+    "url": "/React-movie/static/js/runtime~main.2fdcd1fa.js"
+  },
+  {
+    "revision": "f49a003ef72e39e7c03a",
+    "url": "/React-movie/static/js/2.dd4bcf2b.chunk.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/React-movie/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "5de5919a93cba46a09c8df1296a094ee",
+    "url": "/React-movie/static/media/react-movie-logo.5de5919a.svg"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/React-movie/static/media/tmdb_logo.30cd724b.svg"
+  },
+  {
+    "revision": "80e9d8a2ff7fddf4a6dc1309d9e8a3ff",
+    "url": "/React-movie/static/media/search-icon.80e9d8a2.svg"
+  },
+  {
+    "revision": "bdf2a1dc0f66849d332e408e37cda734",
+    "url": "/React-movie/index.html"
+  }
+];
